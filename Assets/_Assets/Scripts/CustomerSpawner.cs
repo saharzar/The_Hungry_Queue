e6c383ca1ac905fc13c3b1efd07dca737809da8a
Huffz <110351695+Huffz55@users.noreply.github.com>
2025-05-22 21:52:30 +0300

    update

--- Assets/_Assets/Scripts/CustomerSpawner.cs
+++ Assets/_Assets/Scripts/CustomerSpawner.cs
@@ -8,8 +8,8 @@ public class CustomerSpawner : MonoBehaviour
     public float minSpawnTime = 2f;
     public float maxSpawnTime = 5f;
 
-    public int minCustomersPerWave = 1; // minimum kaç müþteri gelsin
-    public int maxCustomersPerWave = 3; // maksimum kaç müþteri gelsin
+    public int minCustomersPerWave = 1;
+    public int maxCustomersPerWave = 3;
 
     void Start()
     {
@@ -18,11 +18,12 @@ public class CustomerSpawner : MonoBehaviour
 
     void SpawnCustomerWave()
     {
-        int customerCount = Random.Range(minCustomersPerWave, maxCustomersPerWave + 1); // kaç kiþi gelsin
+        int customerCount = Random.Range(minCustomersPerWave, maxCustomersPerWave + 1);
         for (int i = 0; i < customerCount; i++)
         {
-            Vector3 spawnOffset = new Vector3(0, 0, i * 2f); // Z eksenine doðru kaydýrma
-            Instantiate(customerPrefab, spawnPoint.position + spawnOffset, Quaternion.identity);
+            Vector3 spawnOffset = new Vector3(0, 0, i * 2f); // Z eksenine doðru sýralý
+            Quaternion faceX = Quaternion.Euler(0, 90f, 0); // Müþteri X yönüne baksýn
+            Instantiate(customerPrefab, spawnPoint.position + spawnOffset, faceX);
         }
 
         Invoke("SpawnCustomerWave", Random.Range(minSpawnTime, maxSpawnTime));
